057ec4137e953c13cb5d0f0b087e2b8cbadeb993
Josaphat3 <lifulaj@gmail.com>
2021-12-28 10:55:17 -0500

    Defined index function in views

--- infoapp/views.py
+++ infoapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+import urllib.request
+import json
 # Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        source = urllib.request.urlopen('http://history.openweathermap.org/data/2.5/history/city?q=' + city + '&units=metric&appid=93248baefaecb815a9686b3b21d118af').read()
+
+        data_list = json.loads(source)
+
+        data = {
+            "country_code": str(data_list['sys']['country']),
+            "temp": str(data_list['main']['temp'])+ '°C',
+            "main": str(data_list['weather'][0]['main']),
+            "icon": data_list['weather'][0]['icon']
+        }
+        print(data)
+    else:
+        data = {}
+    
+    return render(request, "index.html", data)
