6e3cf9ceb158168a4d4da9c0257e6bac6bfd9c51
Josaphat3 <lifulaj@gmail.com>
2022-03-01 02:27:29 -0500

    refactored code

--- infoapp/views.py
+++ infoapp/views.py
@@ -1,6 +1,6 @@
+import json
 from django.shortcuts import render
 import urllib.request
-import json
 # Create your views here.
 
 def index(request):
@@ -8,11 +8,10 @@ def index(request):
         dt = request.POST['dt']
 
         # Users will have to provide the unix timestamps for the 5 previous days. five calls will be perfomed, one for each timestamp.
+        #API key can be obtained from https://openweathermap.org/api
 
-        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=75.69&lon=45.42&dt='+ dt + '&units=metric&appid=93248baefaecb815a9686b3b21d118af').read()
-                    
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=75.69&lon=45.42&dt='+ dt + '&units=metric&appid=???????').read()         
         data_list = json.loads(source)
-
         data = {
             "temp": str(data_list['hourly'][0]['temp'])+ '°C',
             "timezone":str(data_list['timezone']), 
@@ -20,5 +19,4 @@ def index(request):
         print(data)
     else:
         data = {}
-    
     return render(request, "index.html", data)
